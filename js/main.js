46c0284984eaa0dd3924ea9a09cda99604a5474d
hansyoung391-design <hansyoung391@gmail.com>
2025-11-23 22:47:20 +0800

    main.js

--- js/main.js
+++ js/main.js
@@ -173,7 +173,7 @@ function openWaze() {
     Contact
   ======================================================= */
 function openWhatsApp(phoneNumber) {
-    const message = "https://kad-jemputan-kahwin.vercel.app/\n\nHello, maaf menggangu. Saya ingin bertanyakan sesuatu berkenaan majlis perkahwinan ini.";
+    const message = "https://tinyurl.com/kadkahwinHT\n\nHello, maaf menggangu. Saya ingin bertanyakan sesuatu berkenaan majlis perkahwinan ini.";
     const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
     window.open(whatsappUrl, "_blank");  // Opens WhatsApp in a new tab
 }
